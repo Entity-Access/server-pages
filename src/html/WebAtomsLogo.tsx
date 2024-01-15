a833eb0bfeb3e98c69cc5fe2f5a7214e2ef35310
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-01-15 17:31:52 +0530

    Added logo

---
+++ src/html/WebAtomsLogo.tsx
@@ -0,0 +1,20 @@
+import XNode from "./XNode.js";
+
+export const WebAtomsLogo = () => XNode.create("", {}, `<!--
+
+I8,        8        ,8I            88                     db
+\`8b       d8b       d8'            88                    d88b        ,d
+ "8,     ,8"8,     ,8"             88                   d8'\`8b       88
+  Y8     8P Y8     8P   ,adPPYba,  88,dPPYba,          d8'  \`8b    MM88MMM  ,adPPYba,   88,dPYba,,adPYba,   ,adPPYba,
+  \`8b   d8' \`8b   d8'  a8P_____88  88P'    "8a        d8YaaaaY8b     88    a8"     "8a  88P'   "88"    "8a  I8[    ""
+   \`8a a8'   \`8a a8'   8PP"""""""  88       d8       d8""""""""8b    88    8b       d8  88      88      88   \`"Y8ba,
+    \`8a8'     \`8a8'    "8b,   ,aa  88b,   ,a8"      d8'        \`8b   88,   "8a,   ,a8"  88      88      88  aa    ]8I
+     \`8'       \`8'      \`"Ybbd8"'  8Y"Ybbd8"'      d8'          \`8b  "Y888  \`"YbbdP"'   88      88      88  \`"YbbdP"'
+
+
+
+                                   https://www.webatoms.in
+
+
+
+-->`);
