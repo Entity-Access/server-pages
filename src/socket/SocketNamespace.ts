0903e4e6b58c8279283c61fe252be4a2ab1c2896
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-01-15 09:57:56 +0530

    Definitions updated

--- src/socket/SocketNamespace.ts
+++ src/socket/SocketNamespace.ts
@@ -1,12 +1,6 @@
 /* eslint-disable no-console */
-import Inject, { ServiceProvider, injectServiceKeysSymbol } from "@entity-access/entity-access/dist/di/di.js";
-import type SocketService from "./SocketService.js";
-import { parse } from "cookie";
-import { Namespace, Server, Socket } from "socket.io";
-import { camelToChain } from "../core/camelToChain.js";
-import TokenService from "../services/TokenService.js";
-import CookieService from "../services/CookieService.js";
-import SessionUser from "../core/SessionUser.js";
+import { ServiceProvider, injectServiceKeysSymbol } from "@entity-access/entity-access/dist/di/di.js";
+import { Namespace, Socket } from "socket.io";
 
 
 export function Receive(target, key) {
@@ -34,48 +28,9 @@ export function Send(target: typeof SocketNamespace, key) {
 
 export default abstract class SocketNamespace {
 
-    public static server: Namespace;
+    public static namespace: string;
 
-    public static attach(server: Server, name?: string) {
-        if (!name) {
-            name = this.name;
-            if (name.endsWith("Namespace")) {
-                name = name.substring(0, name.length - "Namespace".length);
-            }
-            name = camelToChain(name);
-        }
-        console.log(`Listening Sockets on /${name}`);
-        const s = server.of("/" + name);
-        this.server = s;
-        const tokenService = ServiceProvider.resolve(this,TokenService);
-        const cookieService = ServiceProvider.resolve(this, CookieService);
-        s.on("connection", (socket) => {
-            socket.onAny(async (methodName, ... args: any[]) => {
-                const cookies = parse(socket.request.headers.cookie);
-                const cookie = cookies[tokenService.authCookieName];
-                const sessionUser = await cookieService.createSessionUserFromCookie(cookie, socket.handshake.address);
-                const scope = ServiceProvider.createScope(this);
-                try {
-                    scope.add(SessionUser, sessionUser);
-                    const socketEvent = scope.create(this as any);
-                    (socketEvent as any).socket = socket;
-                    const method = socketEvent[methodName];
-                    const types = method[injectServiceKeysSymbol] as any[];
-                    if (types) {
-                        for (let index = args.length; index < types.length; index++) {
-                            const element = scope.resolve(types[index]);
-                            args.push(element);
-                        }
-                    }
-                    await socketEvent[methodName](... args);
-                } catch (error) {
-                    console.error(error);
-                } finally {
-                    scope.dispose();
-                }
-            });
-        });
-    }
+    public static server: Namespace;
 
     protected room: string;
 
--- src/socket/SocketService.ts
+++ src/socket/SocketService.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import Inject, { RegisterSingleton } from "@entity-access/entity-access/dist/di/di.js";
+import Inject, { RegisterSingleton, ServiceProvider, injectServiceKeysSymbol } from "@entity-access/entity-access/dist/di/di.js";
 import { Http2SecureServer } from "http2";
 import { Server as HttpServer } from "http";
 import { Server as HttpsServer } from "https";
@@ -8,6 +8,9 @@ import { parse } from "cookie";
 import { Server, Socket } from "socket.io";
 import CookieService from "../services/CookieService.js";
 import TokenService from "../services/TokenService.js";
+import SocketNamespace from "./SocketNamespace.js";
+import { camelToChain } from "../core/camelToChain.js";
+import SessionUser from "../core/SessionUser.js";
 
 @RegisterSingleton
 export default class SocketService {
@@ -24,11 +27,54 @@ export default class SocketService {
         this.server = new Server();
     }
 
-    attach(server: Http2SecureServer | HttpsServer | HttpServer): Server {
+    attach(
+        server: Http2SecureServer | HttpsServer | HttpServer,
+        ns: Array<typeof SocketNamespace>): Server {
 
         this.server.attach(server);
-
+        for (const iterator of ns) {
+            this.attachNamespace(iterator);
+        }
         return this.server;
+    }
+    attachNamespace(iterator: typeof SocketNamespace, name = iterator.namespace) {
+        if (!name) {
+            name = iterator.name;
+            if (name.endsWith("Namespace")) {
+                name = name.substring(0, name.length - "Namespace".length);
+            }
+            name = camelToChain(name);
+        }
+        console.log(`Listening Sockets on /${name}`);
+        const s = this.server.of("/" + name);
+        const { tokenService, cookieService } = this;
+        s.on("connection", (socket) => {
+            socket.onAny(async (methodName, ... args: any[]) => {
+                const cookies = parse(socket.request.headers.cookie);
+                const cookie = cookies[tokenService.authCookieName];
+                const sessionUser = await cookieService.createSessionUserFromCookie(cookie, socket.handshake.address);
+                const scope = ServiceProvider.createScope(this);
+                try {
+                    scope.add(SessionUser, sessionUser);
+                    const socketEvent = scope.create(iterator as any) as SocketNamespace;
+                    iterator.server = s;
+                    (socketEvent as any).socket = socket;
+                    const method = socketEvent[methodName];
+                    const types = method[injectServiceKeysSymbol] as any[];
+                    if (types) {
+                        for (let index = args.length; index < types.length; index++) {
+                            const element = scope.resolve(types[index]);
+                            args.push(element);
+                        }
+                    }
+                    await socketEvent[methodName](... args);
+                } catch (error) {
+                    console.error(error);
+                } finally {
+                    scope.dispose();
+                }
+            });
+        });
     }
 
 }
