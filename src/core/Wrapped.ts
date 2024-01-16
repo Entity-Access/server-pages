3d56d0e93fa8528636d9d279e41b3058574f60eb
akashlocal <ackava@gmail.com>
2024-01-16 11:52:47 +0530

    Adding methods

--- src/core/Wrapped.ts
+++ src/core/Wrapped.ts
@@ -1,16 +1,51 @@
 import { IncomingMessage } from "http";
 import { Http2ServerRequest } from "http2";
+import SessionUser from "./SessionUser.js";
 
 export type WrappedRequest = (IncomingMessage | Http2ServerRequest) & ( {
-    get host(): string
+    get host(): string;
+
+    get sessionUser(): Promise<SessionUser>;
+
+    get query(): { [key: string]: string};
+
+    get cookies(): { [key: string]: string};
+
 });
 
-export const Wrapped = (req: IncomingMessage | Http2ServerRequest) => {
-    const wr = req as Http2ServerRequest;
-    if (typeof wr.authority === "undefined") {
-        Object.defineProperty(wr, "host", {
-            get: () => wr.headers[":authority"] ?? wr.headers["host"]
-        });
+const methods = {
+    host() {
+        return this.headers[":authority"] ?? this.headers["host"];
+    },
+    query(this: WrappedRequest) {
+        const u = new URL(this.url, "http://nowhere.com");
+        const items = {};
+        for (const [key, value] of u.searchParams.entries()) {
+            items[key] = value;
+        }
+        return items;
+    },
+    cookies(this: WrappedRequest) {
+        const cookie = this.headers.cookie;
+        
+    },
+    sessionUser(this: WrappedRequest) {
+
     }
-    return wr as WrappedRequest;
+};
+
+export const Wrapped = (req: IncomingMessage | Http2ServerRequest) => {
+    for (const key in methods) {
+        if (Object.prototype.hasOwnProperty.call(methods, key)) {
+            const element = methods[key];
+            Object.defineProperty(req, key, {
+                get() {
+                    Object.defineProperty(this, key, { value: element(), enumerable: true, writable: false });
+                },
+                enumerable: true,
+                configurable: true
+            });
+        }
+    }
+    return req as WrappedRequest;
 }
