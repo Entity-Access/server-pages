74ab45ce08b62b56e00ca71f6c421fa2c33d931d
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-02-14 09:52:04 +0530

    Added detailed error

--- src/ServerPages.ts
+++ src/ServerPages.ts
@@ -126,9 +126,18 @@ export default class ServerPages {
             });
 
             if (createSocketService) {
-                const socketServer = new Server(httpServer);
+                const socketServer = new Server(httpServer, {
+                    
+                });
                 const ss = ServiceProvider.resolve(this, SocketService as any) as SocketService;
                 await (ss as any).attach(socketServer);
+
+                socketServer.engine.on("connection_error", (err) => {
+                    console.log(err.req);      // the request object
+                    console.log(err.code);     // the error code, for example 1
+                    console.log(err.message);  // the error message, for example "Session ID unknown"
+                    console.log(err.context);  // some additional error context                    
+                });
 
                 if (protocol === "http2" || protocol === "http2NoTLS") {
                     httpServer.prependListener("stream", (stream, headers) => {
