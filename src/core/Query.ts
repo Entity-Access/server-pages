7958e6a726b5e3d67e869e68af867ace18808f52
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-10-24 14:43:10 +0530

    Decorators fixed

--- src/core/Query.ts
+++ src/core/Query.ts
@@ -2,7 +2,17 @@ import { prepareSymbol } from "../decorators/Prepare.js";
 import Page from "../Page.js";
 
 export const Query = (page, name) => {
-    (page[prepareSymbol] ??= []).push((p: Page) => {
-        p[name] = p.query[name];
+    // (page[prepareSymbol] ??= []).push((p: Page) => {
+    //     p[name] = p.query[name];
+    // })
+
+    Object.defineProperty(page, name, {
+        enumerable: true,
+        get() {
+            const value = this.query[name];
+            Object.defineProperty(this, name, { value });
+            return value;
+        }
     })
+
 };
--- src/core/Route.ts
+++ src/core/Route.ts
@@ -2,7 +2,12 @@ import { prepareSymbol } from "../decorators/Prepare.js";
 import Page from "../Page.js";
 
 export const Route = (page, name) => {
-    (page[prepareSymbol] ??= []).push((p: Page) => {
-        p[name] = p.route[name];
+    Object.defineProperty(page, name, {
+        enumerable: true,
+        get() {
+            const value = this.route[name];
+            Object.defineProperty(this, name, { value });
+            return value;
+        }
     })
 };
