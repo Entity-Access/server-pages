56e228bc3093a45a1cee328a4076714dc99182da
Akash Kava <39438041+ackava@users.noreply.github.com>
2024-12-09 18:15:43 +0530

    Added default

--- src/core/Wrapped.ts
+++ src/core/Wrapped.ts
@@ -227,7 +227,7 @@ const extendResponse = (A: typeof ServerResponse | typeof Http2ServerResponse) =
                 return new Promise<void>((resolve) => this.end(resolve));
             }
 
-            sendReader(this: UnwrappedResponse, status: number, headers: OutgoingHttpHeaders, readable: Readable, signal?: AbortSignal) {
+            sendReader(this: UnwrappedResponse, status: number, headers: OutgoingHttpHeaders = {}, readable: Readable, signal?: AbortSignal) {
                 const encodings = (this.req as WrappedRequest).acceptEncodings;
                 if (encodings.includes("gzip")) {
                     headers["content-encoding"] = "gzip";
