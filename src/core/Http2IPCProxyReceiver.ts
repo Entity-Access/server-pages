5438c63e419905785c49669a2f37e8315cb63c1d
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-06-09 16:58:58 +0530

    setting default http

--- src/core/Http2IPCProxyReceiver.ts
+++ src/core/Http2IPCProxyReceiver.ts
@@ -64,7 +64,7 @@ export default class Http2IPCProxyReceiver {
 
             socket[remoteAddressSymbol] = ipAddress;
             console.log(tokens);
-            (socket as any).alpnProtocol = alpnProtocol;
+            (socket as any).alpnProtocol = alpnProtocol || "http/1.1";
             this.forward.emit("secureConnection", socket);
         } catch (error) {
             // console.error(error);
