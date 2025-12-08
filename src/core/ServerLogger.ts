0d48e46dae8982429cd13914c81547a26b61c4a8
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-12-08 10:23:23 +0530

    compilation fixed

--- src/core/ServerLogger.ts
+++ src/core/ServerLogger.ts
@@ -23,11 +23,11 @@ export default class ServerLogger {
     reportError({ url, serverID = void 0, error = void 0, info = void 0, userAgent = void 0, ip = void 0}) {
         const cause = error.cause?.stack ?? error.cause?.toString();
         const at = (function getStack() {
-            const obj = {};
+            const obj = { stack : void 0};
             if ("captureStackTrace" in Error) {
                 Error.captureStackTrace(obj, getStack); // Exclude getStack from the trace
             }
-            return obj?.stack;
+            return obj.stack;
         })();
         console.error(JSON.stringify({ url, serverID, error: error.stack ?? error.toString(), cause, info, at }));
     }
