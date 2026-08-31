00af3d65d5849e71d8cd6740db79559cf8c54f30
Akash Kava <39438041+ackava@users.noreply.github.com>
2026-08-31 11:03:22 +0530

    query rename fixed

--- src/core/Query.ts
+++ src/core/Query.ts
@@ -1,11 +1,10 @@
 import type Page from "../Page.js";
-import { Decorators } from "./Decorators.js";
 
-const query = (target, name, vc?: (v) => any): any => {
+const query = (target, name, queryName = name, vc?: (v) => any): any => {
     const defaultValue = Symbol.for(`query-${name}`);
     Object.defineProperty(target, name, {
         get(this: Page) {
-            let value = this.request.query[name];
+            let value = this.request.query[queryName];
             if (value !== void 0) {
                 if (vc) {
                     value = vc(value);
@@ -24,11 +23,11 @@ const query = (target, name, vc?: (v) => any): any => {
     })
 }
 
-const queryCI = (target, name, vc?: (v) => any): any => {
+const queryCI = (target, name, queryName = name, vc?: (v) => any): any => {
     const defaultValue = Symbol.for(`query-${name}`);
     Object.defineProperty(target, name, {
         get(this: Page) {
-            let value = this.request.queryCaseInsensitive[name];
+            let value = this.request.queryCaseInsensitive[queryName];
             if (value !== void 0) {
                 if (vc) {
                     value = vc(value);
@@ -51,52 +50,52 @@ const queryCI = (target, name, vc?: (v) => any): any => {
 export const Query = (page, name?) => {
 
     if (name === void 0) {
-        return (p, n) => query(p, n);
+        return (p, n) => query(p, n, page);
     }
 
-    return query(page, name);
+    return query(page, name, name);
 };
 
 Query.asBoolean = (page, name?) => {
     if (name === void 0) {
-        return (p, n) => query(p, n, (v) => /true|yes/i.test(v));
+        return (p, n) => query(p, n, page, (v) => /true|yes/i.test(v));
     }
-    return query(page, name, (v) => /true|yes/i.test(v));
+    return query(page, name, name, (v) => /true|yes/i.test(v));
 }
 
 Query.asNumber = (page, name?) => {
     if (name === void 0) {
-        return (p, n) => query(p, n, (v) => Number(v));
+        return (p, n) => query(p, n, page, (v) => Number(v));
     }
-    return query(page, name, (v) => Number(v));
+    return query(page, name, name, (v) => Number(v));
 }
 
 Query.asBigInt = (page, name?) => {
     if (name === void 0) {
-        return (p, n) => query(p, page, (v) => BigInt(v));
+        return (p, n) => query(p, n, page, (v) => BigInt(v));
     }
-    return query(page, name, (v) => BigInt(v));
+    return query(page, name, name, (v) => BigInt(v));
 }
 
 Query.caseInsensitive = (page, name?) => {
 
     if (name === void 0) {
-        return (p, n) => queryCI(p, n);
+        return (p, n) => queryCI(p, n, page);
     }
 
-    return queryCI(page, name);
+    return queryCI(page, name, name);
 };
 
 Query.caseInsensitiveAsBoolean = (page, name?) => {
     if (name === void 0) {
-        return (p, n) => queryCI(p, n, (v) => /true|yes/i.test(v));
+        return (p, n) => queryCI(p, n, page, (v) => /true|yes/i.test(v));
     }
-    return queryCI(page, name, (v) => /true|yes/i.test(v));
+    return queryCI(page, name, name, (v) => /true|yes/i.test(v));
 }
 
 Query.caseInsensitiveAsNumber = (page, name?) => {
     if (name === void 0) {
-        return (p, n) => queryCI(p, n, (v) => Number(v));
+        return (p, n) => queryCI(p, n, page, (v) => Number(v));
     }
-    return queryCI(page, name, (v) => Number(v));
+    return queryCI(page, name, name, (v) => Number(v));
 }
